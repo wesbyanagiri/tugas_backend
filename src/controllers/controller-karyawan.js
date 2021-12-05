3b936fe4e3165c8cd4a44e4cb28118f3df7e8467
wesbyanagiri <wesbyana.giri14@gmail.com>
2021-12-05 22:23:34 +0800

    First Upload Project

---
+++ src/controllers/controller-karyawan.js
@@ -0,0 +1,117 @@
+const config = require('../configs/database');
+const mysql = require('mysql');
+const pool = mysql.createPool(config);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
+module.exports ={
+    // Ambil data semua karyawan
+    getDataKaryawan(req,res){
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM tb_karyawan;
+                `
+            , function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil ambil data!',
+                    data: results 
+                });
+            });
+            connection.release();
+        })
+    },
+    // Ambil data karyawan berdasarkan ID
+    getDataKaryawanByID(req,res){
+        let id = req.params.id;
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM tb_karyawan WHERE id = ?;
+                `
+            , [id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil ambil data!',
+                    data: results
+                });
+            });
+            connection.release();
+        })
+    },
+    // Simpan data karyawan
+    addDataKaryawan(req,res){
+        let data = {
+            name : req.body.name,
+            address : req.body.address
+        }
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                INSERT INTO tb_karyawan SET ?;
+                `
+            , [data],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil tambah data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    // Update data karyawan
+    editDataKaryawan(req,res){
+        let dataEdit = {
+            name : req.body.name,
+            address : req.body.address
+        }
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                UPDATE tb_karyawan SET ? WHERE id = ?;
+                `
+            , [dataEdit, id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil edit data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    // Delete data karyawan
+    deleteDataKaryawan(req,res){
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                DELETE FROM tb_karyawan WHERE id = ?;
+                `
+            , [id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil hapus data!'
+                });
+            });
+            connection.release();
+        })
+    }
+}
---
+++ tb_karyawan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Des 2021 pada 14.48
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_slembong`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES
+(2, 'Waw', 'Denpasar', '2021-12-05 13:43:19', '2021-12-05 13:43:19');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
